565314bdfdbd2fcf4576afb17c4c09d55b0203df
Rytiggy <rytiggy@gmail.com>
2016-02-25 09:44:58 -0500

    Chat Client Server

---
+++ ChatClient.java
@@ -0,0 +1,169 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+
+/**
+Dev by: Ryan Mason, Arther Burgin, Chris Dumlao, Bre Dionne
+due Date: 4/4/14
+Title: Chat Client
+This client will be used to send messages to the server and receive them.
+****IMPORTANT YOU NEED TO PUT THE IP OF THE COMPUTER THAT YOU ARE RUNNING THE SERVER ON****
+
+*/
+
+
+public class ChatClient{
+   private final int PORT = 16789;
+   private final String  HOST = "localhost";
+   final JTextArea LOG;
+   final JTextField INPUT;
+   JPanel panel;
+   Socket s;
+   OutputStream os;
+   ObjectOutputStream out;
+   InputStream is;
+   ObjectInputStream ois;
+  
+   public ChatClient(){
+   
+   
+   
+   //creation of the gui
+      JFrame window = new JFrame();
+      panel = new JPanel(new BorderLayout());
+      JButton sendButton = new JButton("Send");
+      sendButton.setEnabled(false);
+   
+   
+   
+      JMenuBar menuBar = new JMenuBar();
+     
+      JMenu fileMenu = new JMenu("File");
+      menuBar.add(fileMenu);
+   
+      JMenuItem exitMenu = new JMenuItem("Exit");
+      fileMenu.add(exitMenu);
+      
+      window.setJMenuBar(menuBar);
+
+   
+   
+   // Log (main text on top)
+      LOG = new JTextArea();
+      JScrollPane scrollPaneLOG = new JScrollPane(LOG);
+      LOG.setRows(25);
+      LOG.setEditable(false);
+      
+      // INPUT (user input on bottom)
+      INPUT = new JTextField();
+      JScrollPane scrollPaneINPUT = new JScrollPane(INPUT);
+      INPUT.setMinimumSize(new Dimension(250, 10));
+      INPUT.setPreferredSize(new Dimension(250, 10));   
+      
+      panel.add(scrollPaneLOG , BorderLayout.NORTH);
+      panel.add(scrollPaneINPUT , BorderLayout.CENTER);
+      panel.add(sendButton , BorderLayout.EAST);
+      
+          
+      window.add(panel);
+      window.setVisible(true);
+      window.pack();
+      window.setLocationRelativeTo(null);
+      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   
+   
+   
+   
+      try{
+         s = new Socket(HOST, PORT);
+         os = s.getOutputStream();
+      
+         sendButton.setEnabled(true);
+      }
+      catch(UnknownHostException ukh){
+      
+      }
+      catch(IOException ioe){
+      
+      }
+      
+      
+            
+      sendButton.addActionListener(
+            new ActionListener(){
+            
+               public void actionPerformed(ActionEvent ae){
+               
+                 // TEXT_INPUT: takes in the users input and sends it to the server\
+                  String senderMsg = INPUT.getText();
+                  try {
+                     System.out.println("ABOUT TO SEND: " +senderMsg);
+                     out.writeObject(senderMsg);
+                     out.flush();
+                     INPUT.setText("");  
+                  }
+                  catch(UnknownHostException uhe) {
+                     append("Unable to connect to host.");
+                  
+                  }
+                  catch(IOException ie) {   
+                  
+                  }
+               }
+            });
+     
+      exitMenu.addActionListener(
+            new ActionListener(){
+            
+               public void actionPerformed(ActionEvent ae){
+                  System.exit(0);
+               }
+            });
+   
+        
+      try{
+         // Create output stream
+         out = new ObjectOutputStream(os);
+      
+         
+         // Create input stream
+         is = s.getInputStream();
+         ois = new ObjectInputStream(is); 
+         
+         
+         Object obj;
+         
+         while(true){
+         
+            obj = ois.readObject();
+         
+            append(obj.toString());
+         }
+      
+      }
+      catch(IOException ioe){
+         append("Server Offline...");
+         JOptionPane.showMessageDialog(null, "Server Offline...");
+         System.exit(0);
+                     
+      }
+      catch(ClassNotFoundException cnfe){
+         append(" 2" +cnfe.getMessage());
+      }
+   
+   }
+   public static void main(String[] args){
+   
+      ChatClient cc = new ChatClient();
+   }
+   
+  
+   public void append(String s)
+   {
+      LOG.append(s + "\n");
+      panel.scrollRectToVisible(LOG.getBounds());
+      LOG.setCaretPosition(LOG.getText().length());
+   }      
+}
---
+++ ChatServer.java
@@ -0,0 +1,144 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+/**
+Dev by: Ryan Mason, Arther Burgin, Chris Dumlao, Bre Dionne
+due Date: 4/4/14
+Title: Chat Server
+This Server will be used to receive messages from the client and then send it to all other clients.
+*/
+
+public class ChatServer{
+   final int PORT = 16789;
+   ServerSocket ss = null;
+   Object message;
+   
+   public Vector<ClientThread> ctVector = new Vector<ClientThread>();
+   public Vector<ObjectOutputStream> clients = new Vector<ObjectOutputStream>();
+   public Vector<Socket> sockets = new Vector<Socket>();
+   String timeStamp = new SimpleDateFormat("hh:mm:ss").format(Calendar.getInstance().getTime());
+
+   
+   public ChatServer(){
+     
+   
+     //server GUI
+      JFrame window = new JFrame();
+      JPanel middle = new JPanel(new GridLayout(1,1));
+     
+      JLabel jtField = new  JLabel(); 
+   
+   
+      try  {
+      
+         jtField.setText("This IP is: "+ InetAddress.getLocalHost());
+      }
+      catch(UnknownHostException uhe){
+         System.err.println(uhe.getMessage());
+      }   
+      middle.add(jtField);
+      window.add(middle);
+      window.setVisible(true);
+      window.pack();
+      window.setLocationRelativeTo(null);
+      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         //end of gui
+     
+     
+     
+     
+      try{
+         ss = new ServerSocket(16789);
+         Socket cs = null;
+         String ip;
+         
+         // waits for client to connect, starts thread, adds to client Vector
+         while(true){
+            cs = ss.accept();
+         
+            ClientThread ct = new ClientThread(cs);
+            ct.start();
+            ctVector.add(ct);
+            System.out.println("Message from " +getIP(cs));
+         }
+      }
+      catch(IOException ioe){
+         System.err.println(ioe.getMessage());
+      }
+   
+   }
+
+   public static void main(String[] args){
+      ChatServer cs = new ChatServer();
+   }
+
+   
+   class ClientThread extends Thread {
+      Socket cs;
+      
+      public ClientThread(Socket cs){
+         this.cs = cs;
+      }
+         
+      public void run() {
+         
+         OutputStream os;
+         ObjectOutputStream out;
+         
+         try{
+            // takes in the string from the client Socket
+            InputStream is = cs.getInputStream();  
+            ObjectInputStream ois = new ObjectInputStream(is);  
+            os = cs.getOutputStream();
+            out = new ObjectOutputStream(os); 
+            
+            clients.add(out);
+            Object obj;
+                     
+            //print it to the client socket
+             
+            while(true){    
+               // read the objects from ois to obj
+               obj = ois.readObject(); 
+            
+            // Determine what kind of object we got
+               if(obj instanceof String){
+                  message = (String)obj;
+               
+               //sends the message to all the clients
+               
+                  for(int i=0; i < clients.size(); i++){
+                     ObjectOutputStream temp = clients.get(i);
+                     try{
+                        System.out.println("Size: " +clients.size() +" Trying to send to client# "  + i);
+                        temp.writeObject("("+timeStamp+ ") " + getIP(cs) + ": " + message);
+                        temp.flush();
+                     }
+                     catch(IOException ioe){
+                        System.out.println("Client Disconnected #"  + i);
+                        clients.remove(i);
+                     }         
+                  }  
+               }
+            }
+         }
+         catch(ClassNotFoundException CNFE){
+         }
+         catch(IOException ioe){
+                  
+         }
+      }
+   } 
+   
+   public String getIP(Socket s){
+      String ip = s.getInetAddress().toString();
+      ip = ip.substring(1,ip.length());
+   
+      return ip;
+   }        
+}
